98e9ddd24cdd191ea25773c73b7b0f7bf3ede233
Tomasz Guzik <tomek@tguzik.com>
2014-05-18 18:23:21 +0200

    Documentation and additional annotations (target, retention) for OwnershipTransferredToTarget

--- src/main/java/com/tguzik/annotations/DiscardObjectAfterInvocation.java
+++ src/main/java/com/tguzik/annotations/DiscardObjectAfterInvocation.java
@@ -7,8 +7,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Indicates that the object with this method should be discarded after invoking
- * this method.
+ * Indicates that the object with this method, or a certain parameter, should be
+ * discarded after invoking this method.
  * 
  * @author Tomasz Guzik <tomek@tguzik.com>
  * @since 0.1
--- src/main/java/com/tguzik/annotations/OwnershipTransferredToTarget.java
+++ src/main/java/com/tguzik/annotations/OwnershipTransferredToTarget.java
@@ -1,5 +1,11 @@
 package com.tguzik.annotations;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * Parameter object annotated by this value will have its lifecycle managed by
  * invoked method.
@@ -7,6 +13,9 @@ package com.tguzik.annotations;
  * @author Tomasz Guzik <tomek@tguzik.com>
  * @since 0.1
  */
+@Documented
+@Retention( RetentionPolicy.CLASS )
+@Target( value = ElementType.PARAMETER )
 public @interface OwnershipTransferredToTarget {
 
 }
